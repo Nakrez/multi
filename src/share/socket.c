e35602b90c6fc32226ffe33d96e15e0db3739dbf
Baptiste COVOLATO <covola_b@epita.fr>
2013-04-14 14:10:19 +0200

    Add documentation to socket processing (share)

--- src/share/socket.c
+++ src/share/socket.c
@@ -1,30 +1,58 @@
 #include <share/socket.h>
 
+/**
+** @brief Write buffer @a data of size @a n in a file descriptor @a fd
+**
+** @param fd    The file descriptor where you want to write @a data
+** @param data  The data you want to write in @a fd
+** @param n     The size in byte you want to write in @a fd
+**
+** @return      0 if everything went well
+                -1 else
+*/
 static int write_to_fd(int fd, const char *data, size_t n)
 {
+    /* The amount of byte left to write */
     size_t left_byte = n;
+    /* The amount of byte written */
     size_t written_byte;
 
     while (left_byte > 0)
     {
+        /* Try to write left byte */
         if ((written_byte = write(fd, data, left_byte)) <= 0)
             return -1;
 
+        /* Update values */
         left_byte -= written_byte;
         data += written_byte;
     }
 
-    return left_byte;
+    return 0;
 }
 
+/**
+** @brief Get the size of a file @a file
+**
+** @param file  The file you want to know the size
+**
+** @return      The size of the file @a file
+*/
 static size_t get_file_size(FILE *file)
 {
+    /*
+    FIXME : Save the previous position from the file an restore it
+    Needed since we are not sure that the cursor was at the begining
+    */
     size_t size;
 
+    /* Seek to the end of the file */
     fseek(file, 0L, SEEK_END);
 
+    /* Get the size */
     size = ftell(file);
 
+    /* Go back to the begining */
     fseek(file, 0L, SEEK_SET);
 
     return size;
@@ -39,22 +67,31 @@ int send_file(int socket_fd, const char *input_name)
 
     char buffer[1024];
 
+    /* Open the input file */
     if ((file = fopen(input_name, "r")) == NULL)
     {
-        printf("Cannot open file: %s", input_name);
+        ERROR_MSG("Cannot open file: %s", input_name);
         return -1;
     }
 
+    /* Get the size of the file */
     file_size = get_file_size(file);
 
-    // Sending file size to server
+    /* Sending file size to server */
     write_to_fd(socket_fd, (char *)&file_size, sizeof (int));
 
     do
     {
+        /* Read from input file */
         read_bytes = fread(buffer, 1, 1024, file);
+
+        /* Send the buffer read to the fd */
         if (write_to_fd(socket_fd, buffer, read_bytes))
-            printf("Error writting file");
+        {
+            fclose(file);
+            return -2;
+        }
+
     } while (read_bytes);
 
     fclose(file);
@@ -71,23 +108,31 @@ int recv_file(int socket_fd, const char *output_name)
 
     char buffer[1024];
 
+    /* Open the output file */
     if ((file = fopen(output_name, "w")) == NULL)
     {
-        printf("Cannot open file: %s", output_name);
+        ERROR_MSG("Cannot open file: %s", output_name);
         return -1;
     }
 
+    /* Get the file size to read from socket */
     if (read(socket_fd, (char *)&bytes_to_read, sizeof (int)) != 4)
     {
-        printf("Cannot retrieve file size\n");
-        return -1;
+        ERROR_MSG("Cannot retrieve file size\n");
+        return -2;
     }
 
     do
     {
+        /* Read data from socket */
         read_bytes = read(socket_fd, buffer, 1024);
+
+        /* Write read byte to the file */
         fwrite(buffer, 1, read_bytes, file);
+
+        /* Update byte left to read */
         bytes_to_read -= read_bytes;
+
     } while (bytes_to_read);
 
     fclose(file);
--- src/share/socket.h
+++ src/share/socket.h
@@ -1,3 +1,10 @@
+/**
+** @file src/share/socket.h
+** @brief All socket transmission related function
+** @date 04/13/2013
+** @author Baptiste COVOLATO <b.covolato@gmail.com>
+*/
+
 #ifndef SOCKET_H
 # define SOCKET_H
 
@@ -7,7 +14,32 @@
 
 # include <sys/socket.h>
 
+# include <share/error.h>
+
+/**
+** @brief Send a file to a file descriptor (can be socket or file)
+**
+** @param socket_fd     The fd to which you want to send your file
+                        (used in @a multi with socket fd but can be file fd)
+** @param input_name    The name of the file you want to send
+**
+** @return              0 if everything went well
+                        -1 if the @a input_file can not be opened
+                        -2 if a write error happened
+*/
 int send_file(int socket_fd, const char *input_name);
+
+/**
+** @brief Receive a file from a file descriptor (can be socket or file)
+**
+** @param socket_fd     The fd from which you want to read the file
+                        (used in @a multi with socket fd but can be file fd)
+** @param output_name   The path where you want to save the received file
+**
+** @return              0 if everything went well
+                        -1 if the @a output_file can not be opened
+                        -2 if a read error happened
+*/
 int recv_file(int socket_fd, const char *output_name);
 
 #endif /* !SOCKET_H */
