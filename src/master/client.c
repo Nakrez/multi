b06a9f75f184b3db856ece3a2b338720b17217a6
Baptiste Covolato <b.covolato@gmail.com>
2013-04-15 01:55:02 +0200

    Improve client code organisation

--- src/master/client.c
+++ src/master/client.c
@@ -3,6 +3,72 @@
 /* #define IP "88.176.106.132" */
 # define IP "127.0.0.1"
 
+/* FIXME doc */
+static int client_preprocess(config_t *config)
+{
+
+    printf("Preprocessing file %s\n", config->file->input_file);
+
+    if (preprocess(config->file->input_file, config->file->output_file))
+    {
+        config_free(&config);
+        return 1;
+    }
+
+    printf("Preprocessing done\n");
+    printf("Sending file to server for compilation\n");
+
+    if (send_file(config->socket_fd, config->file->output_file) < 0)
+    {
+        ERROR_MSG("Error: Can not send file to the server\n");
+        close(config->socket_fd);
+
+        full_compilation(config->file->input_file, config->file->output_file);
+
+        /* Never reached */
+    }
+
+    printf("File sent waiting for response\n");
+
+    return 0;
+}
+
+/* FIXME doc */
+static int client_retrieve_data(config_t *config)
+{
+    /* TODO get compiler return from server */
+    /* TODO retrieve compiler stdout */
+    /* TODO retrieve compiler stderr */
+
+    if (recv_file(config->socket_fd, config->file->output_file) < 0)
+    {
+        ERROR_MSG("Error: Can not receive compiled file from the server\n");
+        close(config->socket_fd);
+
+        full_compilation(config->file->input_file, config->file->output_file);
+
+        /* Never reached */
+    }
+
+    printf("File received\n");
+
+    return 0;
+}
+
+/* FIXME doc */
+static int core_client(config_t *config)
+{
+    if (client_preprocess(config))
+        return 1;
+
+    if (client_retrieve_data(config))
+        return 1;
+
+    config_free(&config);
+
+    return 0;
+}
+
 int launch_client(int argc, char *argv[])
 {
     config_t *config;
@@ -21,41 +87,6 @@ int launch_client(int argc, char *argv[])
         return 1;
     }
 
-    printf("Preprocessing file %s\n", config->file->input_file);
-
-    if (preprocess(config->file->input_file, config->file->output_file))
-        goto exit;
-
-    printf("Preprocessing done\n");
-
-    printf("Sending file to server for compilation\n");
-
-    if (send_file(config->socket_fd, config->file->output_file) < 0)
-    {
-        ERROR_MSG("Error: Can not send file to the server\n");
-        close(config->socket_fd);
-
-        full_compilation(config->file->input_file, config->file->output_file);
-
-        /* Never reached */
-    }
-
-    printf("File sent waiting for response\n");
-
-    if (recv_file(config->socket_fd, config->file->output_file) < 0)
-    {
-        ERROR_MSG("Error: Can not receive compiled file from the server\n");
-        close(config->socket_fd);
-
-        full_compilation(config->file->input_file, config->file->output_file);
-
-        /* Never reached */
-    }
-
-    printf("File received\n");
-
-exit:
-    config_free(&config);
-
-    return 0;
+    /* Process file with server */
+    return core_client(config);
 }
