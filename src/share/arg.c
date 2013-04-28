fc178637a8e0786a62eb4e7ddcb7447b09e92fe8
Baptiste Covolato <b.covolato@gmail.com>
2013-04-28 16:54:01 +0200

    Add documentation of src/share/arg.h

--- src/share/arg.c
+++ src/share/arg.c
@@ -1,9 +1,33 @@
 #include <share/arg.h>
 
+/* TODO : Same code in master/arg.c clean it */
+static int is_source(char *src)
+{
+    int len = strlen(src);
+
+    if (len < 3)
+        return 0;
+
+    return src[0] != '-' &&
+           ((len > 2 && !strcmp(src + len - 2, ".c")) ||
+           (len > 3 && !strcmp(src + len - 3, ".cc")) ||
+           (len > 4 && !strcmp(src + len - 4, ".cpp")));
+}
+/* END TODO*/
+
+/**
+** @brief Give the length of concatenation of argv array
+**
+** @param   argc    The number of arguments
+** @param   argv    The array containing the arguments
+**
+** @return          The result size
+*/
 static int argv_size_concate(int argc, char **argv)
 {
     int result = 0;
 
+    /* +1 for the separating space or the final \0 */
     for (int i = 0; i < argc; ++i)
         result += strlen(argv[i]) + 1;
 
@@ -28,11 +52,14 @@ char **dup_argv(int argc, char **argv)
     return new_argv;
 }
 
-void add_argv(int argc, char ***argv, char *str)
+int add_argv(int argc, char ***argv, char *str)
 {
-    *argv = realloc(*argv, (argc + 1) * sizeof (char *));
+    if ((*argv = realloc(*argv, (argc + 1) * sizeof (char *))) == NULL)
+        return -1;
 
     (*argv)[argc] = str;
+
+    return 0;
 }
 
 int concate_argv(int argc, char **argv, char **result)
@@ -43,13 +70,12 @@ int concate_argv(int argc, char **argv, char **result)
 
     size = argv_size_concate(argc, argv);
 
-    /* TODO Handle error */
-    *result = malloc(size);
+    if ((*result = malloc(size)) == NULL)
+        return -1;
 
     for (int i = 0; i < argc; ++i)
     {
         size_arg = strlen(argv[i]);
-        /* strncpy */
         strncpy(*result + pos, argv[i], size_arg);
         pos += size_arg;
 
@@ -60,7 +86,6 @@ int concate_argv(int argc, char **argv, char **result)
 
     return size;
 }
-
 int split_argv(char *argv, char ***result)
 {
     int old = -1;
@@ -70,12 +95,14 @@ int split_argv(char *argv, char ***result)
 
     char *temp = argv;
 
+    /* TODO Handle errors */
     *result = malloc((argc + 1) * sizeof (char *));
 
     while (1)
     {
         if (*temp == ' ' || !*temp)
         {
+            /* TODO Handle errors */
             (*result)[i] = malloc(new - old);
             strncpy((*result)[i], argv + old + 1, new - old);
             (*result)[i][new - old - 1] = 0;
@@ -95,21 +122,6 @@ int split_argv(char *argv, char ***result)
 
     return argc;
 }
-
-/* TODO : Same code in master/arg.c clean it */
-static int is_source(char *src)
-{
-    int len = strlen(src);
-
-    if (len < 3)
-        return 0;
-
-    return src[0] != '-' &&
-           ((len > 2 && !strcmp(src + len - 2, ".c")) ||
-           (len > 3 && !strcmp(src + len - 3, ".cc")) ||
-           (len > 4 && !strcmp(src + len - 4, ".cpp")));
-}
-/* END TODO*/
 
 void update_argv_file(int argc, char **argv, char *input_file, char *output_file)
 {
--- src/share/arg.h
+++ src/share/arg.h
@@ -24,16 +24,49 @@
 */
 char **dup_argv(int argc, char **argv);
 
-/* FIXME doc */
-void add_argv(int argc, char ***argv, char *str);
+/**
+** @brief   Add argumtent to an array of argument. Be carefull the function does
+            does not increment @a argc
+**
+** @param   argc    The number of argument in the array
+** @param   argv    The array containing the arguments
+** @param   str     The argument to add to the array
+**
+** @return  0 if everything went well, -1 else
+*/
+int add_argv(int argc, char ***argv, char *str);
 
-/* FIXME doc */
+/**
+** @brief Concatenate array of arguments in one string
+**
+** @param   argc    The number of argument in the array
+** @param   argv    The array containing the arguments
+** @param   result  The resulting string of the concatenation
+**
+** @return  The size of the string if everything went well, -1 else
+*/
 int concate_argv(int argc, char **argv, char **result);
 
-/* FIXME doc */
+/**
+** @brief Split string into an array of argument
+**
+** @param   argv    The string to split
+** @param   result  The resulting array. Desallocate this array is the duty of
+                    the caller
+**
+** @return  The size of the resulting array. Error not handled yet
+*/
 int split_argv(char *argv, char ***result);
 
-/* FIXME doc */
+/**
+** @brief   Find input and output file in the array of argument and replace it
+            with @a input_file and @a output_file
+**
+** @param   argc        The number of argument in the array
+** @param   argv        The array containing the arguments
+** @param   input_file  The new input file name
+** @param   output_file The new output file name
+*/
 void update_argv_file(int argc, char **argv, char *input_file,
                       char *output_file);
 
